38bc5337dbe9f68f4e7e752888ece7bd77dcad3e
Paul Truong <mrpaultruong@gmail.com>
2019-05-13 21:52:52 +1000

    More layout tweaks

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styles from "./Logo.module.css"
 const Logo = ({ height }) => (
-    <svg className={styles.logo} width="auto" height={height} viewBox="0 0 395 415" fill="black" xmlns="http://www.w3.org/2000/svg">
+    <svg className={styles.logo} height={height} viewBox="0 0 395 415" fill="black" xmlns="http://www.w3.org/2000/svg">
         <rect y="15" width="125" height="400" />
         <circle cx="270" cy="125" r="125" />
     </svg>
--- src/components/Menu/MainMenu.js
+++ src/components/Menu/MainMenu.js
@@ -6,83 +6,35 @@ import MediaQuery from "react-responsive"
 class MainMenu extends React.Component {
   constructor() {
     super()
-
-    this.state = {
-      isMenuOpen: false,
-    }
-  }
-
-  toggleMenu = () => {
-    if (this.state.isMenuOpen === true) {
-      this.setState({ isMenuOpen: false })
-    } else {
-      this.setState({ isMenuOpen: true })
-    }
   }
 
   render() {
     return (
       <div>
-        <MediaQuery query="(max-width:700px)">
-          <nav>
-            <button
-              onClick={this.toggleMenu}
-              className={mainMenuStyles.mainMenu__button}
-            >
-              Menu
-            </button>
-            {this.state.isMenuOpen ? (
-              <ul className={mainMenuStyles.mainMenu}>
-                <li className={mainMenuStyles.mainMenu__item}>
-                  <Link className={mainMenuStyles.mainMenu__link} to="/">
-                    Home
-                  </Link>
-                </li>
-                <li className={mainMenuStyles.mainMenu__item}>
-                  <Link className={mainMenuStyles.mainMenu__link} to="/about">
-                    About
-                  </Link>
-                </li>
-                <li className={mainMenuStyles.mainMenu__item}>
-                  <Link className={mainMenuStyles.mainMenu__link} to="/writing">
-                    Writing
-                  </Link>
-                </li>
-                <li className={mainMenuStyles.mainMenu__item}>
-                  <Link className={mainMenuStyles.mainMenu__link} to="/#contact">
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-            ) : null}
-          </nav>
-        </MediaQuery>
-        <MediaQuery query="(min-width:700px)">
-          <nav>
-            <ul className={mainMenuStyles.mainMenu}>
-              <li className={mainMenuStyles.mainMenu__item}>
-                <Link className={mainMenuStyles.mainMenu__link} to="/">
-                  Home
+        <nav>
+          <ul className={mainMenuStyles.mainMenu}>
+            <li className={mainMenuStyles.mainMenu__item}>
+              <Link className={mainMenuStyles.mainMenu__link} to="/">
+                Home
                 </Link>
-              </li>
-              <li className={mainMenuStyles.mainMenu__item}>
-                <Link className={mainMenuStyles.mainMenu__link} to="/about">
-                  About
+            </li>
+            <li className={mainMenuStyles.mainMenu__item}>
+              <Link className={mainMenuStyles.mainMenu__link} to="/about">
+                About
                 </Link>
-              </li>
-              <li className={mainMenuStyles.mainMenu__item}>
-                <Link className={mainMenuStyles.mainMenu__link} to="/writing">
-                  Writing
+            </li>
+            <li className={mainMenuStyles.mainMenu__item}>
+              <Link className={mainMenuStyles.mainMenu__link} to="/writing">
+                Writing
                 </Link>
-              </li>
-              <li className={mainMenuStyles.mainMenu__item}>
-                <Link className={mainMenuStyles.mainMenu__link} to="/#contact">
-                  Contact
+            </li>
+            <li className={mainMenuStyles.mainMenu__item}>
+              <Link className={mainMenuStyles.mainMenu__link} to="/#contact">
+                Contact
                 </Link>
-              </li>
-            </ul>
-          </nav>
-        </MediaQuery>
+            </li>
+          </ul>
+        </nav>
       </div>
     )
   }
